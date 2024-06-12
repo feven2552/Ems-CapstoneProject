446787edb365a53ab446edb9b39a8d550a835aca
feven <feven.25yi@gmail.com>
2024-06-12 03:46:54 -0700

    added catagory and employee, modified route and adminRoute

--- EmployeeManagmentSystem/src/Components/EmployeeLogin.jsx
+++ EmployeeManagmentSystem/src/Components/EmployeeLogin.jsx
@@ -3,8 +3,7 @@ import './style.css'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
-const Login = () => {
-
+const EmployeeLogin = () => {
     const [values, setValues] = useState({
         email: '',
         password: ''
@@ -14,11 +13,11 @@ const Login = () => {
     axios.defaults.withCredentials = true;
     const handleSubmit = (event) => {
         event.preventDefault()
-        axios.post('http://localhost:3000/auth/adminlogin', values)
+        axios.post('http://localhost:3000/employee/employee_login', values)
         .then(result => {
             if(result.data.loginStatus) {
                 localStorage.setItem("valid", true)
-                navigate('/dashboard')
+                navigate('/employee_detail/'+result.data.id)
             } else {
                 setError(result.data.Error)
             }
@@ -47,7 +46,7 @@ const Login = () => {
                 <button className='btn btn-success w-100 rounded-0 mb-2'>Log in</button>
                 <div className='mb-1'> 
                     <input type="checkbox" name="tick" id="tick" className='me-2'/>
-                    <label htmlFor="password">You are Agree with terms & conditions</label>
+                 <label htmlFor="password">You are Agree with terms & conditions</label>
                 </div>
             </form>
         </div>
@@ -55,4 +54,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default EmployeeLogin
